216d60fe0b2239d6f992aed91cce4d2969d5d8e0
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-01 14:50:54 +0000

    use Carbon

--- main.php
+++ main.php
@@ -1,9 +1,13 @@
 <?php
 
+require __DIR__.'/vendor/autoload.php';
+
+use Carbon\Carbon;
+
 // https://stackoverflow.com/a/8450359/4233593
 function processDate(Traversable $elements)
 {
-    $date = DateTime::createFromFormat('m/d/y', $elements[2]->nodeValue);
+    $date = Carbon::createFromFormat('m/d/y', $elements[2]->nodeValue);
     return $date->format('Y-m-d');
 }
 
